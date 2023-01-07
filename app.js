cb6a73814c0f7b1d1df022245a1bb45b5775059d
Adam Alexander <adamjalexander88@gmail.com>
2023-01-07 15:05:02 +0000

    ok

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const menu = document.getElementById('menu');
 const toggle = document.getElementById('toggle');
-const icon = document.getElementById('menu-icon');
 const body = document.querySelector('body');
+const subItem = document.querySelector('#sub-item');
+const subMenu = document.querySelector('#sub');
 
 toggle.addEventListener('click', () => {
     menu.classList.toggle('active')
     body.classList.toggle('active');
-  
-    if( icon.src = 'images/icon-menu.svg'){
-        icon.src = 'images/icon-close-menu.svg'
-    } else{
-        icon.src =  'images/icon-menu.svg';
-    }
+    toggle.classList.toggle('active')
+    
+})
+
+subItem.addEventListener('click', () => {
+    subMenu.classList.toggle('active')
 })
